494c2d8f7c0d128e70953d803591a97c0871c28a
مجله بیت کوین <working.magazine4you@gmail.com>
2026-07-30 14:56:40 +0330

    Update SplashActivity.kt

--- app/src/main/java/ir/magazine4you/bitcoin/SplashActivity.kt
+++ app/src/main/java/ir/magazine4you/bitcoin/SplashActivity.kt
@@ -2,22 +2,16 @@ package ir.magazine4you.bitcoin
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class SplashActivity : AppCompatActivity() {
 
-    private val splashDelayMillis = 1500L
-
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
 
-        Handler(Looper.getMainLooper()).postDelayed({
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
-            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
-        }, splashDelayMillis)
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
     }
 }
